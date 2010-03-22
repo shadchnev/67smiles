4dec22ec7f23ad084067fef1f28b23af74710c34
Evgeny Shadchnev <evgenyshadchnev@evgeny-shadchnevs-macbook-air.local>
2010-03-22 17:57:32 +0000

    broken build to finish on home computer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,33 @@
 class HomeController < ApplicationController
   
-  def index
-    @cleaners = default_selection
-    return unless search_params_present?
-    postcode = Postcode.find_or_create_by_normalized_value(params[:postcode])
-    flash.now[:error] = "We couldn't locate your postcode. Please try a postcode near you instead" and return unless postcode.id
-    local_cleaners = Cleaner.find_near(postcode)
-    local_cleaners.empty? ? flash.now[:notice] = "Sorry, we don't have any cleaners registered in your area" : @cleaners = local_cleaners
+  def index      
+    begin
+      postcode, skill, date = [build_postcode, build_skill, build_date]
+      @cleaners = Cleaner.find_suitable(:origin => postcode, :skill => skill, :date => date)
+      # local_cleaners.empty? ? flash.now[:notice] = "Sorry, we don't have any cleaners registered in your area" : @cleaners = local_cleaners      
+    rescue Exception => e
+      flash.now[:error] = e.message 
+    ensure
+      @cleaners = default_selection 
+    end
   end
 
-private
+private  
 
+  def build_postcode
+    postcode = Postcode.find_or_create_by_normalized_value(params[:postcode])
+    postcode.id ? postcode : raise("We couldn't locate your postcode. Please try a postcode near you instead")
+  end
+  
+  def build_skill
+    skill = Skill.new do |skill|
+      [:domestic_cleaning, :ironing, :groceries, :pets].each do |s|
+        skill.send("#{s}=", params[s])
+      end
+    end
+    skill.select_all! if skill.empty?
+  end
+    
   def default_selection
     Cleaner.find(:all, :limit => 20, :order => 'created_at desc')
   end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
+  
   test "the truth" do
     assert true
   end
