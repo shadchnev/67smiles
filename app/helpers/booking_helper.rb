e3e4182b15ee01fdb98805853e704d6d0eb4a1f7
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2010-03-28 17:35:00 +0100

    fixed ruby 1.9 utf8 issue

--- app/helpers/booking_helper.rb
+++ app/helpers/booking_helper.rb
@@ -2,7 +2,7 @@ module BookingHelper
   
   def surcharge_select_content(booking)
     # [['Yes (no extra charge)', 1], [raw("No (£#{round_if_possible(booking.cleaner.surcharge)}/hour extra)"), 0]]
-    options_for_select({'Yes (no extra charge)' => '1', raw("No (£#{round_if_possible(booking.cleaner.surcharge)}/hour extra)") => '0'},
+    options_for_select({'Yes (no extra charge)' => '1', raw("No (\u00A3#{round_if_possible(booking.cleaner.surcharge)}/hour extra)") => '0'},
       booking.cleaning_materials_provided? ? '1' : '0')
   end
 end
