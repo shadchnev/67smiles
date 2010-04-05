63623602bbf116a8f25a78389a2037dff2fe5a54
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2010-04-05 16:28:57 +0100

    minor refactoring

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -11,8 +11,7 @@ class ClientsController < ApplicationController
   
   def create
     postcode = find_or_create_postcode
-    params[:client][:address_attributes].delete(:postcode_attributes)
-    params[:client][:user_attributes][:login] = params[:client][:contact_details_attributes][:email]
+    preprocess_params
     @client = Client.new(params[:client])
     @client.address.postcode = postcode    
     if @client.save
@@ -26,8 +25,7 @@ class ClientsController < ApplicationController
   def update
     @client = Client.find(params[:id])
     @client.address.postcode = find_or_create_postcode
-    params[:client][:address_attributes].delete(:postcode_attributes)
-    params[:client][:user_attributes][:login] = params[:client][:contact_details_attributes][:email]    
+    preprocess_params
     if @client.update_attributes(params[:client])
       flash[:notice] = "Your profile has been updated."
       redirect_to('/')
@@ -42,6 +40,11 @@ class ClientsController < ApplicationController
   end
 
 private
+
+  def preprocess_params
+    params[:client][:address_attributes].delete(:postcode_attributes)
+    params[:client][:user_attributes][:login] = params[:client][:contact_details_attributes][:email]        
+  end
 
   def find_or_create_postcode
     Postcode.find_or_create_by_normalized_value(params[:client][:address_attributes][:postcode_attributes][:value])
