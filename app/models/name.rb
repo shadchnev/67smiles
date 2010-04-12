d2caa89e1bc7f5aea2afd43504f9e0521d48a62d
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2010-04-12 23:50:19 +0100

    removed dr from the honorific

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,6 +1,6 @@
 class Name < ActiveRecord::Base 
   
-  validates_presence_of :honorific, :message => "^Please specify whether you are Mr, Miss, Mrs, Ms or Dr"
+  validates_presence_of :honorific, :message => "^Please specify whether you are Mr, Miss, Mrs or Ms"
   validates_presence_of :first_name, :message => "^Please enter your first name (e.g. Emma)"
   validates_presence_of :last_name, :message => "^Please enter your last name (e.g. Cameron)"
   
