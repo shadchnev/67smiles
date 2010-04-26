0cb52e156fab753178731616d771d059e2dce8a6
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2010-04-26 19:36:36 +0100

    updated sms content

--- test/unit/sms_content_test.rb
+++ test/unit/sms_content_test.rb
@@ -5,14 +5,14 @@ class SmsContentTest < ActiveSupport::TestCase
   test "booking enquiry with an integer cost" do
     booking = Booking.build!(:start_time => Time.parse('05/04/2010 12:00'), :end_time => Time.parse('05/04/2010 16:00'))
     SmsContent.stubs(:booking_reply_deadline).returns '22:12'
-    assert_equal "Job in E1W: 05 April 12:00 - 16:00 with own cleaning stuff. Will pay 48 pounds. Accept? Reply 'yes' or 'no' before 22:12", SmsContent.booking_enquiry(booking)
+    assert_equal "VarsityCleaners: job offer in E1W. 05 April 12:00 - 16:00 with own cleaning stuff. Will pay 48 pounds. Accept? Reply 'yes' or 'no' before 22:12", SmsContent.booking_enquiry(booking)
   end
   
   test "booking enquiry with a non-integer cost" do
     cleaner = Cleaner.build(:rate => 11.3)
     booking = Booking.build!(:start_time => Time.parse('22/03/2010 12:00'), :end_time => Time.parse('22/03/2010 16:00'), :cleaning_materials_provided => true, :cleaner => cleaner)
     SmsContent.stubs(:booking_reply_deadline).returns '22:12'
-    assert_equal "Job in E1W: 22 March 12:00 - 16:00 (cleaning stuff provided). Will pay 45.20 pounds. Accept? Reply 'yes' or 'no' before 22:12", SmsContent.booking_enquiry(booking)
+    assert_equal "VarsityCleaners: job offer in E1W. 22 March 12:00 - 16:00 (cleaning stuff provided). Will pay 45.20 pounds. Accept? Reply 'yes' or 'no' before 22:12", SmsContent.booking_enquiry(booking)
   end
   
   test "booking accepted" do
