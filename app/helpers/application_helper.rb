d51016cabb460d6969168394df2603c479b80f5f
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2010-05-02 21:51:00 +0100

    uni in profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,40 @@ module ApplicationHelper
     ((value - value.round).abs < 0.1 ? value.round.to_i : value) if value
   end
   
+  def university(email)
+    case email
+    when /ic.ac.uk$/; 'Imperial College London'
+    when /imperial.ac.uk$/; "Imperial College London"      
+    when /westminster.ac.uk$/; 'Westminster University'
+    when /marjon.ac.uk$/; 'University College Marjon'
+    when /londonmet.ac.uk$/; 'London Metropolitan University'
+    when /ucl.ac.uk$/; 'University College London'
+    when /bournemouth.ac.uk$/; 'Bournemouth University'
+    when /live.mdx.ac.uk$/; 'Middlesex University'
+    when /csm.arts.ac.uk$/; 'Central Saint Martins University of the Arts'
+    when /\bchester.ac.uk$/; 'University of Chester'
+    when /manchester.ac.uk$/; 'University of Manchester'
+    when /heythrop.ac.uk$/; 'Heythrop College'
+    when /heythropcollege.ac.uk$/; 'Heythrop College'
+    when /open.ac.uk$/; 'Open University'
+    when /qmul.ac.uk$/; 'Queen Mary, University of London'
+    when /kent.ac.uk$/; 'University of Kent'
+    when /soas.ac.uk$/; 'School of Oriental and African Studies, University of London'
+    when /bcu.ac.uk$/; 'Birmingham City University'
+    when /uel.ac.uk$/; 'University of East London'
+    when /edgehill.ac.uk$/; 'Edge Hill University'
+    when /rgu.ac.uk$/; 'Robert Gordon University'
+    when /anglia.ac.uk$/; 'Anglia Ruskin University'
+    when /ljmu.ac.uk$/; 'Liverpool John Moores University'
+    when /uclan.ac.uk$/; 'University of Central Lancashire'
+    when /kcl.ac.uk$/; 'King\'s College London'
+    when /herts.ac.uk$/; 'University of Hertfordshire'
+    when /caledonian.ac.uk$/; 'Glasgow Caledonian University'
+    when /city.ac.uk$/; 'City University London'
+    else email.split("@").last
+    end
+  end
+  
   def navigational_link(key)
     case key
     when :about               then link_to "About", "/about"
